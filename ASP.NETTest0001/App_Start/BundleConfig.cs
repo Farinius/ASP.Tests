1fea0a20c77a6d616b28e203e1d38d788e129fa7
Eppensteiner Rene <rene.eppensteiner@gmail.com>
2016-02-23 21:34:45 +0100

    Blub

--- ASP.NETTest0001/App_Start/BundleConfig.cs
+++ ASP.NETTest0001/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ASP.NETTest0001
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-Theme.css",
                       "~/Content/site.css"));
         }
     }
